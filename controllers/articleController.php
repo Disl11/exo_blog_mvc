5c728a339bbda0fff7db4c5d934beb2170738c48
Brian Farnier <brian.pro1993@gmail.com>
2025-10-02 16:06:23 +0200

    add vews

--- controllers/articleController.php
+++ controllers/articleController.php
@@ -13,11 +13,14 @@ class ArticleController
     public function index()
     {
         $articles = $this->model->getArticles();
+
+        require 'views/articles.php';
     }
 
     public function show($id)
     {
 
-        $article = $this->model->getArticle();
+        $article = $this->model->getArticle($id);
+        require 'views/article.php';
     }
 }
